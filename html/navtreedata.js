57e6829e3d229da30a8b17afa416308a00b11356
Travis CI <travis@travis-ci.org>
2019-07-24 09:39:21 +0000

    Deploy code docs to GitHub Pages Travis build: 344
    Commit: f049df49b5b963c39e926dbc6aa74d6fa1976d13

---
+++ html/navtreedata.js
@@ -0,0 +1,22 @@
+var NAVTREE =
+[
+  [ "AngularCorrelationExtraction", "index.html", [
+    [ "AngularCorrelationExtraction", "md_README.html", null ],
+    [ "Files", null, [
+      [ "File List", "files.html", "files" ],
+      [ "Globals", "globals.html", [
+        [ "All", "globals.html", null ],
+        [ "Functions", "globals_func.html", null ],
+        [ "Variables", "globals_vars.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"angCorr_8cpp.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
